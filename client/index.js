2758ee9dde11c583a15f330bb13230ad1888cf74
Renan Martins <martins@fujisan.co.jp>
2017-10-26 10:02:30 -0700

    Add apollo client and provider for React root component

--- client/index.js
+++ client/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Works with any FE framework (keeps the Apollo Store)
+import ApolloClient from 'apollo-client';
+// Integrates with React:
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(
