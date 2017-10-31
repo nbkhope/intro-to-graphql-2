1df5a5a99b7ff836e655df3e60e8d31723146f7d
Renan Martins <martins@fujisan.co.jp>
2017-10-31 10:41:28 -0700

    Implement song deletion

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import gql from 'graphql-tag';
 // Integrates React with Apollo Client; used to make query
 import { graphql } from 'react-apollo';
 
@@ -8,9 +9,26 @@ import query from '../queries/fetchSongs';
 import Spinner from './Spinner';
 
 class SongList extends Component {
+  onSongDelete(songId) {
+    this.props.mutate({
+      variables: {
+        id: songId,
+      },
+    })
+      .then(response => {
+        console.log(`Deleted song with id ${songId} successfully.`);
+
+        this.props.data.refetch();
+      })
+  }
+
   renderItems() {
     return this.props.data.songs.map(song => (
-      <li key={song.id} className="collection-item">{song.title}</li>
+      <li key={song.id} className="collection-item">
+        <Link to={`/songs/${song.id}`}>{song.title}</Link>
+        <a href="#!" className="secondary-content"><i className="material-icons" onClick={() => this.onSongDelete(song.id)}>delete</i></a>
+
+      </li>
     ));
   }
 
@@ -33,5 +51,15 @@ class SongList extends Component {
   }
 }
 
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
 // graphql(query) returns a function
-export default graphql(query)(SongList);
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
