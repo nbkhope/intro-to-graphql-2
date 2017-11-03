8fe0e57bc3e91140df895c6269a4305cf020b942
Renan Martins <martins@fujisan.co.jp>
2017-11-03 10:40:47 -0700

    Disable lyric content input when request has been made to prevent massive lyric creation

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -7,40 +7,41 @@ class LyricCreate extends Component {
     super(props);
 
     this.state = {
-      content: ''
+      content: '',
+      loading: false,
     };
   }
 
   onSubmit(event) {
-    console.log('onSubmit. song is', this.props.song);
     event.preventDefault();
     console.log('Submitting lyric line...', this.state.content);
-
-    this.props.mutate({
-      variables: {
-        songId: this.props.songId,
-        content: this.state.content,
-      },
-      optimisticResponse: {
-        __typename: 'Mutation',
-        addLyricToSong: {
-          id: this.props.songId,
-          title: this.props.song.title,
-          lyrics: this.props.song.lyrics.concat({
-            id: new Date().getTime() + Math.random(),
-            content: this.state.content, likes: 0,
-            __typename: 'LyricType'
-          }),
-          __typename: 'SongType',
+    this.setState({ loading: true }, () => {
+      this.props.mutate({
+        variables: {
+          songId: this.props.songId,
+          content: this.state.content,
+        },
+        optimisticResponse: {
+          __typename: 'Mutation',
+          addLyricToSong: {
+            id: this.props.songId,
+            title: this.props.song.title,
+            lyrics: this.props.song.lyrics.concat({
+              id: new Date().getTime() + Math.random(),
+              content: this.state.content, likes: 0,
+              __typename: 'LyricType'
+            }),
+            __typename: 'SongType',
+          }
         }
-
-      }
-    })
+      })
       .then(response => {
         console.log('Submitted lyric line successfully', response);
 
-        this.setState({ content: '' });
+        this.setState({ content: '', loading: false });
       });
+    });
+
   }
 
   render() {
@@ -53,6 +54,7 @@ class LyricCreate extends Component {
           id="content"
           value={this.state.content}
           onChange={(event) => this.setState({ content: event.target.value })}
+          disabled={this.state.loading}
         />
       </form>
     );
